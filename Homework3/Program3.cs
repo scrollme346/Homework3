abf52f15f00e9c50af4354f01be9d8b53f7bbbc6
scrollme346 <133518123+scrollme346@users.noreply.github.com>
2023-11-03 01:40:48 +0200

    Program3.cs

---
+++ Homework3/Program3.cs
@@ -0,0 +1,39 @@
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Первое число:");
+        int num = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Второе число:");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Уточните арифметическое действие(+,-,*,/,%-остаток)");
+        string znak = Console.ReadLine();
+
+        if (znak == "+")//если знак + 
+        {
+            int sum = num + num1;// то плюсуем
+            Console.WriteLine($"Ваше число: {sum}");//выводим результат
+        }
+        else if (znak == "-") 
+        {
+            int minus = num - num1;
+            Console.WriteLine($"Ваше число: {minus}");
+        }
+        else if (znak == "*") 
+        {
+            int mnosh = num * num1;
+            Console.WriteLine($"Ваше число: {mnosh}");
+        }
+        else if (znak == "/")
+        {
+            float delen = num / num1;
+            Console.WriteLine($"Ваше число: {delen}");
+        }
+        else if (znak == "%")
+        {
+            int ostat = num % num1;
+            Console.WriteLine($"Остаток от числа: {ostat}");
+        }       
+    }
+}
+//калькулятор
