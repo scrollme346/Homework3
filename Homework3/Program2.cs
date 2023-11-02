46a3f3572bc549e06c3d8f2e0f651891595c98a9
scrollme346 <133518123+scrollme346@users.noreply.github.com>
2023-11-02 22:34:52 +0200

    Program2.cs

---
+++ Homework3/Program2.cs
@@ -0,0 +1,18 @@
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        if (num % 2 == 0)// если процентный остаток num равняется нулю то
+        {
+            int mnosh = num * 3;// num умножаем на три
+            Console.WriteLine($"Ваше число {num} делится на 2(и является четным) \nУмноженое на три: {mnosh}");//выводим результат
+        }
+        else//если не делится
+        {
+            float delen = num / 2f;//делим на два
+            Console.WriteLine($"Ваше число {num} не делится на два (и не является четным) \nРазделенно на два: {delen}");// выводим результат
+        }
+    }
+}
